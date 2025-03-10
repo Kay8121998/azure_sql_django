15a6360a7f014d0d6282e36a5ed3c46a40394b73
Kay Yadanar Phyo <kayp1724@gmail.com>
2025-03-10 23:56:37 +0700

    Azure_Workshop_Kay

--- api/models.py
+++ api/models.py
@@ -1,12 +1,21 @@
-from datetime import timezone
+
 from django.db import models
-
-# Create your models here.
-
+from django.utils import timezone
 
 class Store(models.Model):
     store_id = models.IntegerField(unique=True)
     store_location = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)  # Set default value here
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.store_id} - {self.store_location}"
+    
+class Product(models.Model):
+    product_id = models.IntegerField(unique=True)
+    product_name = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)  # Set default value here
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product_id} - {self.product_name}"
